505bfaa27f36bc4ca03695e00f0c2c9dc68ac5d7
mandooplz <kmw4561@gmail.com>
2025-07-01 03:11:49 +0900

    update ProjectBoardView

--- BudClientiOS/Interface/ProjectBoard/ProjectBoardView.swift
+++ BudClientiOS/Interface/ProjectBoard/ProjectBoardView.swift
@@ -7,17 +7,45 @@
 import SwiftUI
 import BudClient
 import Tools
+import os
 
 
 // MARK: View
 struct ProjectBoardView: View {
-    var projectBoardRef: ProjectBoard
+    @Bindable var projectBoardRef: ProjectBoard
     
     init(_ objectRef: ProjectBoard) {
         self.projectBoardRef = objectRef
     }
     
     var body: some View {
-        Text("This is ProjectBoard")
+        NavigationStack {
+            List {
+                ForEach(projectBoardRef.projects, id: \.value) { projectId in
+                    if let project = projectId.ref {
+                        Text(project.name)
+                    } else {
+                        Text("Unknown Project")
+                    }
+                }
+            }
+            .task {
+                projectBoardRef.setUp()
+                await projectBoardRef.startObserving()
+            }
+            .navigationTitle("Projects")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        Task {
+                            await projectBoardRef.createEmptyProject()
+                            print(projectBoardRef.projects.count)
+                        }
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
     }
 }
